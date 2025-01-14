b2a8f94d1c6c2f0a5ff1968f864f9f74d2eb9257
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-14 10:33:00 +0530

    lec 9

--- Learning-Node-1/Server/index.js
+++ Learning-Node-1/Server/index.js
@@ -2,16 +2,46 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+// const myServer = http.createServer((req, res) => {
+//   if (req.url === '/favicon.ico') return res.end();
+//   const log = `${Date.now()}: ${req.url} New Request Received\n `;
+//   const myUrl = url.parse(req.url, true);
+//   console.log(myUrl);
+//   fs.appendFile("log.txt", log, (err, data) => {
+//     // res.end("Hello From Server");
+//     switch (myUrl.pathname) {
+//       case "/":
+//         res.end("Homepage");
+//         break;
+//       case "/about":
+//         const username = myUrl.query.myname
+//         res.end( `Hii, ${username}`);
+//         break;
+//       case "/search":
+//         const search = myUrl.query.search_query;
+//         res.end("Here are your results for "+search);
+//       default:
+//         res.end("404 Not Found");
+//     }
+//   });
+// });
+
+// myServer.listen(8000, () => console.log("Server Started!"));
+
+
+
+// Lecture 9 :
+
 const myServer = http.createServer((req, res) => {
   if (req.url === '/favicon.ico') return res.end();
-  const log = `${Date.now()}: ${req.url} New Request Received\n `;
+  const log = `${Date.now()}: ${req.method} ${req.url} New Request Received\n `;
   const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
+  
   fs.appendFile("log.txt", log, (err, data) => {
     // res.end("Hello From Server");
     switch (myUrl.pathname) {
       case "/":
-        res.end("Homepage");
+        if (req.method === 'GET') res.end("Homepage");
         break;
       case "/about":
         const username = myUrl.query.myname
@@ -20,6 +50,12 @@ const myServer = http.createServer((req, res) => {
       case "/search":
         const search = myUrl.query.search_query;
         res.end("Here are your results for "+search);
+      case '/signup':
+        if(req.method === 'GET') res.end('This is a sign up form')
+        else if (req.method === 'POST'){
+          //DB Query
+          res.end("Success");
+        }
       default:
         res.end("404 Not Found");
     }
