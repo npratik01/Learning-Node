b4c8ef246b0476ff9d6d3e12893c5244c348c5eb
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-14 10:07:48 +0530

    lec 8

--- Learning-Node-1/Server/index.js
+++ Learning-Node-1/Server/index.js
@@ -1,17 +1,25 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
+  if (req.url === '/favicon.ico') return res.end();
   const log = `${Date.now()}: ${req.url} New Request Received\n `;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     // res.end("Hello From Server");
-    switch (req.url) {
+    switch (myUrl.pathname) {
       case "/":
         res.end("Homepage");
         break;
       case "/about":
-        res.end("I am Pratik Nikam");
+        const username = myUrl.query.myname
+        res.end( `Hii, ${username}`);
         break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end("Here are your results for "+search);
       default:
         res.end("404 Not Found");
     }
