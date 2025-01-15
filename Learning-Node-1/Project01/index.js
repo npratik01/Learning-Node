6a79e2bdf662cb46b2ef1f0f74cd4ff2f07e1f22
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-15 21:08:59 +0530

    Lec 13

--- Learning-Node-1/Project01/index.js
+++ Learning-Node-1/Project01/index.js
@@ -19,11 +19,26 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/api/users/:id", (req, res) => {
+app.route("/api/users/:id").get((req, res) => {
   const id = Number(req.params.id);
   const user = users.find((user) => user.id === id);
   return res.json(user);
+}).patch((req, res) => {
+  // Edit the User with id
+  return res.json({status: "pending"});
+})
+.delete((req, res) => {
+  // Delete the User with id
+  return res.json({status: "pending"});
 });
+
+
+
+app.post('/api/users', (req, res) => {
+  //TODO: Create new user
+  return res.json({status: "pending"});
+});
+
 
 
 
