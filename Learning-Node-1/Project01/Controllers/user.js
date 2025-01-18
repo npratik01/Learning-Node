8e714a43c2213647b495982702cc54584bfe1ab8
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-18 22:03:07 +0530

    mogodb connection running

---
+++ Learning-Node-1/Project01/Controllers/user.js
@@ -0,0 +1,57 @@
+const User = require("../Models/user");
+
+
+async function handleGetAllUsers(req, res) {
+  const allDbUsers = await User.find({});
+  return res.json(allDbUsers);
+  
+}
+
+async function handleGetUserbyID(req, res){
+  const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: "user not found" });
+    return res.json(user);
+}
+
+async function handleUpdateUserByID(req, res){
+  await User.findByIdAndUpdate(req.params.id, {lastName: "Changed"});
+    return res.json({ status: "Success" });
+}
+
+async function handleDeleteUserByID(req, res){
+  await User.findByIdAndUpdate(req.params.id);
+  return res.json({status: "Success"});
+}
+
+async function handleCreateNewUser(req, res){
+  const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title ||
+    !body.last_name
+  ) {
+    return res.status(400).json({ msg: "All fields are required" });
+  }
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
+  });
+
+  return res.status(201).json({ msg: "success", id: result._id});
+}
+
+module.exports = {
+  handleGetAllUsers,
+  handleGetUserbyID,
+  handleUpdateUserByID,
+  handleDeleteUserByID,
+  handleCreateNewUser,
+}
+
--- Learning-Node-1/Project01/index.js
+++ Learning-Node-1/Project01/index.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const fs = require("fs");
+const {connectMongoDb} = require("./connection");
+
+const {logReqRes} = require("./middlewares");
+
+const userRouter = require("./Routes/user");
+
 const app = express();
 const PORT = 8000;
-const mongoose = require("mongoose");
-const userRouter = require("./Routes/user")
-
 
 // Connection
-mongoose
-  .connect("mongodb://127.0.0.1:27017/youtube-app-1")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("Mongo Error", err));
+connectMongoDb("mongodb://127.0.0.1:27017/youtube-app-1");
 
 // Schema
 // const userSchema = new mongoose.Schema({
@@ -32,7 +31,7 @@ mongoose
 //   gender: {
 //     type: String,
 //   },
-  
+
 // }, {timestamps: true});
 
 // const User = mongoose.model("user", userSchema);
@@ -40,25 +39,9 @@ mongoose
 // Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  fs.appendFile(
-    "log.txt",
-    `\n${Date.now()}: ${req.ip} ${req.method}: ${req.path}\n`,
-    (err, data) => {
-      next();
-    }
-  );
-});
-
-app.use((req, res, next) => {
-  console.log("Hello from middleware2", req.myUserName);
-  //return res.end("Hey");
-  next(); // for calling next function
-});
+app.use(logReqRes("log.txt"));
 
 //Routes:
-app.use("/user", userRouter);
-
-
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
