67bf28700190c7544c14c3309e26ab7b720d1028
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-22 05:58:50 +0530

    custom middleware

--- AngelaBai Projects/3.4 Middleware/index4.js
+++ AngelaBai Projects/3.4 Middleware/index4.js
@@ -1,7 +1,31 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+var bandName = "";
+
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function bandNameGenerator(req, res, next){
+  console.log(req.body);
+  bandName = req.body["street"] + req.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is : </h1><h2> ${bandName}</h2>`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
