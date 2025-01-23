a03b0eb813683c96d8117b1a28f02084bfa11683
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-23 23:14:15 +0530

    crud op with ejs running

--- Sheryians Playlist/CRUD Operations with ejs/app.js
+++ Sheryians Playlist/CRUD Operations with ejs/app.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
   res.render("index");
 });
 
+app.get('/read', (req, res) => {
+  res.render("read");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server started at PORT: ${PORT}`)
