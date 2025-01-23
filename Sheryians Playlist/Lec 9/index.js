12727d2cec57c05ff0b004aef42b2698898884ca
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-23 13:52:56 +0530

    chal raha hain

---
+++ Sheryians Playlist/Lec 9/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () =>{
+  console.log(`Server is running on PORT : ${PORT}`)
+})
