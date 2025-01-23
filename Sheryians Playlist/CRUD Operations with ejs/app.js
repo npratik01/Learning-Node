9c49af82811eb7264179f846df2f11f25546661f
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-23 21:07:35 +0530

    CRUD op with ejs

---
+++ Sheryians Playlist/CRUD Operations with ejs/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server started at PORT: ${PORT}`)
+});
