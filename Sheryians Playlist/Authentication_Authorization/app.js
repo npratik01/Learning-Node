acfa56d3b8e93f1032293d4b82ed423f54cab491
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-24 16:06:37 +0530

    cookie

--- Sheryians Playlist/Authentication_Authorization/app.js
+++ Sheryians Playlist/Authentication_Authorization/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+app.use
+
 app.get("/", (req, res) => {
   res.cookie("name", "Pratik");
   res.send("done");
+});
+
+app.get("/read", (req, res) => {
+  console.log(req.cookies);
+  res.send("read page");
 });
 
 app.listen(PORT, () => {
