c968d66212a07cd7b9318a1c1181286c8177cdb9
Pratik Vijay Nikam <2241038@gcoej.ac.in>
2025-01-24 20:46:57 +0530

    chal raha hain

---
+++ Sheryians Playlist/2Authentication_Authorization/app.js
@@ -0,0 +1,19 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+})
